5aab0f70d9fd1b9dd9c91bf9d30d86a90f329cd5
Md. Jawad Jamil <md.jawadjamil@gmail.com>
2021-08-08 17:48:30 +0600

    Update custom.js

--- custom.js
+++ custom.js
@@ -68,7 +68,7 @@ function math_operation(){
     } else if (last_oper == '/') {
         result = parseFloat(result) / parseFloat(dis2_num)
     } else if (last_oper == '%') {
-        parseFloat(dis2_num)/100
+        result = parseFloat(result) / parseFloat(dis2_num/100)
     }
 }
 
